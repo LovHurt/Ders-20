539bffe49e17495c1282a635a8bc1bbb50f6dfa8
Alican Dağıdır <alicandagidir@hotmail.com>
2023-10-24 02:48:08 +0300

    Add project files.

---
+++ Ders 20/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (number < 100)
+            {
+                if (number >=90 && number < 95)
+                {
+
+                }
+                else 
+                {
+                    
+                }
+
+                if (b)
+                {
+
+                }
+                else 
+                { 
+                
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
